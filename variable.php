bddcd3ae4792c58480cdd81c4c129dd9f9884c9e
errjak <dioadyaw@gmail.com>
2022-03-10 00:38:57 +0700

    Menambahkan File Tugas No 2

---
+++ variable.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Tugas 1
+    </title>
+</head>
+<body>
+
+    <h1>Variable</h1>
+
+    <?php
+    $angka1=3;
+    $angka2=2;
+    echo "<h3>Penjumlahan</h3>";
+    echo "Penjumlahan dari ".$angka1." + ". $angka2." adalah ". $angka1 + $angka2."<br>";
+    echo "<h3>Pengurangan</h3>";
+    echo "Pengurangan dari " . $angka1 . " - " . $angka2 . " adalah " . $angka1 - $angka2 . "<br>";
+    echo "<h3>Perkalian</h3>";
+    echo "Perkalian dari " . $angka1 . " x " . $angka2 . " adalah " . $angka1 * $angka2 . "<br>";
+    ?>
+
+</body>
+
+</html>
