afb9fe2abdb709cebe6c53e1761c9db000ddd303
errjak <dioadyaw@gmail.com>
2022-03-10 00:38:27 +0700

    Menambahkan File Tugas No 1

---
+++ hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Tugas 1
+    </title>
+</head>
+
+<body>
+
+    <h1>Data Diri</h1>
+
+    <?php
+    echo "Nama : Dio Adya Wicaksono<br>NIM : 200411100066";
+    ?>
+
+</body>
+
+</html>
