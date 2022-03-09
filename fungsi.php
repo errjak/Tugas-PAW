9a6526659af3b92db06154009e23df89f4ed8078
errjak <dioadyaw@gmail.com>
2022-03-10 00:40:24 +0700

    Menambahkan File Tugas No 3

---
+++ fungsi.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Tugas 1
+    </title>
+</head>
+
+<body>
+
+    <h1>Function Arguments</h1>
+    <?php
+    function sayHello($nama, $umur)
+    {
+        echo "Halo saya $nama, umur saya adalah $umur tahun<br/>";
+    }
+    sayHello("Dio", 17);
+    sayHello("Adya", 19);
+    sayHello("Wicaksono", 20);
+    ?>
+
+</body>
+
+</html>
